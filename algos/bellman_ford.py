148fa612fae848252f3d74fc4eb0360fe86f7f04
Adam Novotny <adam@adamnovotny.com>
2018-06-20 12:56:31 +0200

    bellman-ford

--- algos/bellman_ford.py
+++ algos/bellman_ford.py
@@ -3,7 +3,8 @@ import heapq
 
 """
 Graph below represents: https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm#/media/File:Dijkstra_Animation.gif
-Requires non-negative edge weights to avoid loops
+Negative edge weights are allowed
+Negative weight cycles not allowed -> graph has no shortest distances
 """
 
 graph = {
@@ -12,32 +13,30 @@ graph = {
     "3": {"1": 9, "2": 10, "4": 11, "6": 2},
     "4": {"2": 15, "3": 11, "6": 2},
     "5": {"4": 6, "6": 9},
-    "6": {"1": 14, "3": 2, "5": 9}
+    "6": {"1": 14, "3": 2, "5": -5}
 }
 
-def dijsktra(graph, source):
+def bellman_ford(graph, source):
     dist = {}
-    visited = []
-    h = []
     for i in graph:
         if i == source:
             dist[i] = 0
-            heapq.heappush(h, (0, source))
         else:
             dist[i] = np.inf
-    while h:
-        node_heap = heapq.heappop(h)
-        u = node_heap[1]
-        dist_to_u = node_heap[0]
+    for i in range(len(graph)):
+        for u in graph:
+            for v in graph[u]:
+                alt_dist = dist[u] + graph[u][v]
+                if dist[v] > alt_dist:
+                    dist[v] = alt_dist
+    # check for cycles
+    for u in graph:
         for v in graph[u]:
             alt_dist = dist[u] + graph[u][v]
             if dist[v] > alt_dist:
-                dist[v] = alt_dist
-            if v not in visited:
-                heapq.heappush(h, (dist[v], v))
-        visited.append(u)
+                raise Exception("Negative cycle")
     return dist
 
 if __name__ == "__main__":
-    dist = dijsktra(graph, "1")
-    assert {'1': 0, '2': 7, '3': 9, '4': 20, '5': 20, '6': 11} == dist
+    dist = bellman_ford(graph, "1")
+    assert {'1': 0, '2': 7, '3': 9, '4': 12, '5': 6, '6': 11} == dist
