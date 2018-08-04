e63fb6e2ab3110ac843521c791189e829d71c5c1
Adam Novotny <adam@adamnovotny.com>
2018-08-04 21:02:12 +0200

    feat: momentum oscilators

--- ml-preprocessing/generate_features.py
+++ ml-preprocessing/generate_features.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import pandas as pd
+import numpy as np
 
 def return_features(df):
     """
@@ -23,12 +24,13 @@ def trend_features(df):
         pandas.DataFrame
     """
     df = macd(df)
-    # TODO MA
-    # TODO Parabolic Stop and Reverse
+    df = ma(df)
+    df = parabolic_sar(df)
     return df
 
 def macd(df):
     """
+    Math reference: https://stockcharts.com/school/doku.php?id=chart_school:technical_indicators:moving_average_convergence_divergence_macd
     Args:
         df: pandas.DataFrame, columns include at least ["close"]
     Returns:
@@ -38,8 +40,9 @@ def macd(df):
     ema_26_day = df["close"].ewm(com=(26-1)/2).mean()
     df["macd_line"] = ema_12_day - ema_26_day
     df["macd_9_day"] = df["macd_line"].ewm(com=(9-1)/2).mean()
+    df["macd_diff"] = df["macd_line"] - df["macd_9_day"]
     # print(df.tail(10)[["date", "close", "macd_line", "macd_9_day"]])
-    chart_macd(df)
+    # chart_macd(df)
     return df
 
 def chart_macd(df):
@@ -60,8 +63,103 @@ def chart_macd(df):
     df.tail(300)[["date", "close"]].plot(x="date", kind="line", ax=ax1)
     ax2 = axes[1]
     ax2.set_title("MACD")
-    df.tail(300)[["date", "macd_line", "macd_9_day"]].plot(x="date", kind="line", ax=ax2)
+    df.tail(300)[["date", "macd_line", "macd_9_day"]].plot(x="date", kind="line", ax=ax2, secondary_y=False)
+    # df.tail(300)[["date", "macd_diff"]].plot(x="date", kind="bar", ax=ax2, secondary_y=True)
     fig.savefig("charts/macd.png")
+
+def ma(df):
+    """
+    Math reference: https://stockcharts.com/school/doku.php?id=chart_school:technical_indicators:moving_averages
+    Args:
+        df: pandas.DataFrame, columns include at least ["close"]
+    Returns:
+        pandas.DataFrame
+    """
+    df["ma_50_day"] = df["close"].rolling(50).mean()
+    df["ma_200_day"] = df["close"].rolling(200).mean()
+    df["ma_50_200"] = df["ma_50_day"] - df["ma_200_day"]
+    # print(df.tail(10)[["date", "close", "ma_50_200"]])
+    # chart_ma(df)
+    return df
+
+def chart_ma(df):
+    """
+    Save chart to charts/ma
+    Args:
+        df: pandas.DataFrame, columns include at least ["date", "close", "ma_50_200"]
+    Returns:
+        None
+    """
+    fig, axes = plt.subplots(1, 1, figsize=(10, 10))
+    fig.tight_layout()
+    plt.suptitle("MSFT Moving Average 50 day - 200 day", fontsize=24)
+    plt.subplots_adjust(left=0.1, top=0.9, right=0.9, hspace=0.4)
+    axes.set_ylabel("$")
+    df.tail(1500)[["date", "close"]].plot(x="date", kind="line", ax=axes, secondary_y=False)
+    df.tail(1500)[["date", "ma_50_day"]].plot(x="date", kind="line", ax=axes, secondary_y=False)
+    df.tail(1500)[["date", "ma_200_day"]].plot(x="date", kind="line", ax=axes, secondary_y=False)
+    df.tail(1500)[["date", "ma_50_200"]].plot(x="date", kind="line", ax=axes, secondary_y=True)
+    fig.savefig("charts/ma.png")
+
+def parabolic_sar(df):
+    """
+    Math reference: https://stockcharts.com/school/doku.php?id=chart_school:technical_indicators:parabolic_sar
+    Args:
+        df: pandas.DataFrame, columns include at least ["close"]
+    Returns:
+        pandas.DataFrame
+    """
+    df["sar"] = np.nan
+    step = 5
+    acc_factor = 0.02
+    uptrend = False
+    prior_sar = max(df.loc[1:step, "close"])
+    extreme_point = min(df.loc[1:step, "close"])
+    for i, row in df.iloc[step:].iterrows():
+        if uptrend:
+            df.at[i, "sar"] = prior_sar + acc_factor*(extreme_point - prior_sar)
+            if df.at[i, "low"] < df.at[i, "sar"]:
+                # reverse to downtrend
+                uptrend = False
+                prior_sar = max(df.loc[i-step:i, "close"])
+                extreme_point = min(df.loc[i-step:i, "close"])
+            else:
+                # continue uptrend
+                if df.at[i, "close"] > extreme_point:
+                    extreme_point = df.at[i, "close"]
+                    acc_factor = min(0.2, acc_factor+0.02)
+        else:
+            df.at[i, "sar"] = prior_sar - acc_factor*(prior_sar - extreme_point)
+            if df.at[i, "high"] > df.at[i, "sar"]:
+                # reverse to uptrend
+                uptrend = True
+                prior_sar = min(df.loc[i-step:i, "close"])
+                extreme_point = max(df.loc[i-step:i, "close"])
+            else:
+                # continue downtrend
+                if df.at[i, "close"] < extreme_point:
+                    extreme_point = df.at[i, "close"]
+                    acc_factor = min(0.2, acc_factor+0.02)
+        prior_sar = df.at[i, "sar"]
+    # chart_sar(df)
+    return df
+
+def chart_sar(df):
+    """
+    Save chart to charts/ma
+    Args:
+        df: pandas.DataFrame, columns include at least ["date", "close", "sar"]
+    Returns:
+        None
+    """
+    fig, axes = plt.subplots(1, 1, figsize=(10, 10))
+    fig.tight_layout()
+    plt.suptitle("MSFT SAR", fontsize=24)
+    plt.subplots_adjust(left=0.1, top=0.9, right=0.9, hspace = 0.4)
+    axes.set_ylabel("$")
+    df.tail(100)[["date", "close"]].plot(x="date", kind="line", ax=axes, secondary_y=False)
+    df.tail(100)[["date", "sar"]].plot(x="date", style=".", ax=axes, secondary_y=False)
+    fig.savefig("charts/sar.png")
 
 def momentum_features(df):
     """
@@ -70,12 +168,81 @@ def momentum_features(df):
     Returns:
         pandas.DataFrame
     """
-    df["5d_momentum"] = df["close"] / df["close"].shift(5)
-    df["21d_momentum"] = df["close"] / df["close"].shift(21)
-    df["60d_momentum"] = df["close"] / df["close"].shift(60)
-    # TODO Stochastic
-    # TODO Commodity Channel (CCI)
-    # TODO RSI
+    df = stochastic_oscillator(df)
+    df = commodity_channel_index(df)
+    df = rsi(df)
+    return df
+
+def stochastic_oscillator(df):
+    """
+    Math reference: https://stockcharts.com/school/doku.php?id=chart_school:technical_indicators:stochastic_oscillator_fast_slow_and_full
+    Args:
+        df: pandas.DataFrame, columns include at least ["close"]
+    Returns:
+        pandas.DataFrame
+    """
+    lookback = 14
+    df["stochastic_oscillator"] = ((df["close"] - df["close"].rolling(lookback).min()) /
+        (df["close"].rolling(lookback).max() - df["close"].rolling(lookback).min())) * 100
+    # chart_stochastic_oscillator(df)
+    return df
+
+def chart_stochastic_oscillator(df):
+    """
+    Save chart to charts/stochastic_oscillator
+    Args:
+        df: pandas.DataFrame, columns include at least ["date", "close", "stochastic_oscillator"]
+    Returns:
+        None
+    """
+    fig, axes = plt.subplots(1, 1, figsize=(10, 10))
+    fig.tight_layout()
+    plt.suptitle("MSFT Stochastic Oscillator", fontsize=24)
+    plt.subplots_adjust(left=0.1, top=0.9, right=0.9, hspace = 0.4)
+    df.tail(100)[["date", "close"]].plot(x="date", kind="line", ax=axes, secondary_y=False)
+    df.tail(100)[["date", "stochastic_oscillator"]].plot(x="date", kind="line", ax=axes, secondary_y=True)
+    fig.savefig("charts/stochastic_oscillator.png")
+
+def commodity_channel_index(df):
+    """
+    Math reference: https://stockcharts.com/school/doku.php?id=chart_school:technical_indicators:commodity_channel_index_cci
+    Args:
+        df: pandas.DataFrame, columns include at least ["open", "high", "low", "close"]
+    Returns:
+        pandas.DataFrame
+    """
+    typical_price = (df["high"] + df["low"] + df["close"]) / 3
+    mean_dev = abs(typical_price - typical_price.rolling(20).mean()).rolling(20).mean()
+    df["cci"] = (typical_price - typical_price.rolling(20).mean()) / (0.15 * mean_dev)
+    # chart_commodity_channel_index(df)
+    return df
+
+def chart_commodity_channel_index(df):
+    """
+    Save chart to charts/commodity_channel_index
+    Args:
+        df: pandas.DataFrame, columns include at least ["date", "close", "stochastic_oscillator"]
+    Returns:
+        None
+    """
+    fig, axes = plt.subplots(1, 1, figsize=(10, 10))
+    fig.tight_layout()
+    plt.suptitle("MSFT Commodity Channel Index (CCI)", fontsize=24)
+    plt.subplots_adjust(left=0.1, top=0.9, right=0.9, hspace = 0.4)
+    df.tail(300)[["date", "close"]].plot(x="date", kind="line", ax=axes, secondary_y=False)
+    df.tail(300)[["date", "cci"]].plot(x="date", kind="line", ax=axes, secondary_y=True)
+    fig.savefig("charts/commodity_channel_index.png")
+
+def rsi(df):
+    """
+    Math reference: https://stockcharts.com/school/doku.php?id=chart_school:technical_indicators:relative_strength_index_rsi
+    Args:
+        df: pandas.DataFrame, columns include at least ["close"]
+    Returns:
+        pandas.DataFrame
+    """
+    
+    # chart_rsi(df)
     return df
 
 def volatility_features(df):
