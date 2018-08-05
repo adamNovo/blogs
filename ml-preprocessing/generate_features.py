56782d74d5d1009606b952e735016cea34a1bdbe
Adam Novotny <adam@adamnovotny.com>
2018-08-05 13:49:15 +0200

    feat: scaling, dim reduction, sampling

--- ml-preprocessing/generate_features.py
+++ ml-preprocessing/generate_features.py
@@ -1,6 +1,9 @@
-import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
+
+produce_charts = False
+if produce_charts:
+    import matplotlib.pyplot as plt    
 
 def return_features(df):
     """
@@ -14,6 +17,11 @@ def return_features(df):
     df["close_to_high"] = df["close"] / df["high"]
     df["close_to_low"] = df["close"] / df["low"]
     df = df.iloc[1:] # first first row: does not have a return value
+    return df
+
+def target_value(df):
+    df["y"] = df["return"].shift(-1)
+    df = df.iloc[:len(df)-1]
     return df
 
 def trend_features(df):
@@ -42,7 +50,8 @@ def macd(df):
     df["macd_9_day"] = df["macd_line"].ewm(com=(9-1)/2).mean()
     df["macd_diff"] = df["macd_line"] - df["macd_9_day"]
     # print(df.tail(10)[["date", "close", "macd_line", "macd_9_day"]])
-    # chart_macd(df)
+    if produce_charts:
+        chart_macd(df)
     return df
 
 def chart_macd(df):
@@ -79,7 +88,8 @@ def ma(df):
     df["ma_200_day"] = df["close"].rolling(200).mean()
     df["ma_50_200"] = df["ma_50_day"] - df["ma_200_day"]
     # print(df.tail(10)[["date", "close", "ma_50_200"]])
-    # chart_ma(df)
+    if produce_charts:
+        chart_ma(df)
     return df
 
 def chart_ma(df):
@@ -141,7 +151,8 @@ def parabolic_sar(df):
                     extreme_point = df.at[i, "close"]
                     acc_factor = min(0.2, acc_factor+0.02)
         prior_sar = df.at[i, "sar"]
-    # chart_sar(df)
+    if produce_charts:
+        chart_sar(df)
     return df
 
 def chart_sar(df):
@@ -184,7 +195,8 @@ def stochastic_oscillator(df):
     lookback = 14
     df["stochastic_oscillator"] = ((df["close"] - df["close"].rolling(lookback).min()) /
         (df["close"].rolling(lookback).max() - df["close"].rolling(lookback).min())) * 100
-    # chart_stochastic_oscillator(df)
+    if produce_charts:
+        chart_stochastic_oscillator(df)
     return df
 
 def chart_stochastic_oscillator(df):
@@ -214,7 +226,8 @@ def commodity_channel_index(df):
     typical_price = (df["high"] + df["low"] + df["close"]) / 3
     mean_dev = abs(typical_price - typical_price.rolling(20).mean()).rolling(20).mean()
     df["cci"] = (typical_price - typical_price.rolling(20).mean()) / (0.15 * mean_dev)
-    # chart_commodity_channel_index(df)
+    if produce_charts:
+        chart_commodity_channel_index(df)
     return df
 
 def chart_commodity_channel_index(df):
@@ -254,15 +267,16 @@ def rsi(df):
         else:
             rs = avg_gains / avg_losses
         df.loc[i, "rsi"] = 100 - 100 / (1 + rs)
-    print(df.tail(20)[["date", "close", "rsi"]])
-    chart_rsi(df)
+    # print(df.tail(20)[["date", "close", "rsi"]])
+    if produce_charts:
+        chart_rsi(df)
     return df
 
 def chart_rsi(df):
     """
     Save chart to charts/rsi
     Args:
-        df: pandas.DataFrame, columns include at least ["date", "close", "stochastic_oscillator"]
+        df: pandas.DataFrame, columns include at least ["date", "close", "rsi"]
     Returns:
         None
     """
@@ -284,10 +298,75 @@ def volatility_features(df):
     df["5d_volatility"] = df["return"].rolling(5).std()
     df["21d_volatility"] = df["return"].rolling(21).std()
     df["60d_volatility"] = df["return"].rolling(60).std()
+    df = bollinger_bands(df)
+    df = average_true_range(df)
+    return df
+
+def bollinger_bands(df):
+    """
+    Math reference: https://stockcharts.com/school/doku.php?id=chart_school:technical_indicators:bollinger_bands
+    Args:
+        df: pandas.DataFrame, columns include at least ["close"]
+    Returns:
+        pandas.DataFrame
+    """
     df["bollinger"] = ((df["close"] - df["close"].rolling(21).mean()) / 
         2 * df["close"].rolling(21).std())
-    # TODO Average true range
+    if produce_charts:
+        chart_bollinger(df)
     return df
+
+def chart_bollinger(df):
+    """
+    Save chart to charts/bollinger
+    Args:
+        df: pandas.DataFrame, columns include at least ["date", "close", "bollinger"]
+    Returns:
+        None
+    """
+    fig, axes = plt.subplots(1, 1, figsize=(10, 10))
+    fig.tight_layout()
+    plt.suptitle("MSFT Bollinger Bands", fontsize=24)
+    plt.subplots_adjust(left=0.1, top=0.9, right=0.9, hspace = 0.4)
+    df.tail(100)[["date", "close"]].plot(x="date", kind="line", ax=axes, secondary_y=False)
+    df.tail(100)[["date", "bollinger"]].plot(x="date", kind="line", ax=axes, secondary_y=True)
+    fig.savefig("charts/bollinger.png")
+
+def average_true_range(df):
+    """
+    Math reference: https://stockcharts.com/school/doku.php?id=chart_school:technical_indicators:average_true_range_atr
+    Args:
+        df: pandas.DataFrame, columns include at least ["high" ,"low, "close"]
+    Returns:
+        pandas.DataFrame
+    """
+    high_vs_low = df["high"] - df["low"]
+    high_vs_prev_close = df["high"] - df["close"].shift(-1)
+    low_vs_prev_close = df["low"] - df["close"].shift(-1)
+    tr = high_vs_low.to_frame("high_vs_low")
+    tr["high_vs_prev_close"] = high_vs_prev_close
+    tr["low_vs_prev_close"] = low_vs_prev_close
+    tr["tr"] = tr.max(axis=1)
+    df["atr"] = tr["tr"].rolling(14).mean()
+    if produce_charts:
+        chart_average_true_range(df)
+    return df
+
+def chart_average_true_range(df):
+    """
+    Save chart to charts/average_true_range
+    Args:
+        df: pandas.DataFrame, columns include at least ["date", "close", "atr"]
+    Returns:
+        None
+    """
+    fig, axes = plt.subplots(1, 1, figsize=(10, 10))
+    fig.tight_layout()
+    plt.suptitle("MSFT Average True Range (ATR)", fontsize=24)
+    plt.subplots_adjust(left=0.1, top=0.9, right=0.9, hspace = 0.4)
+    df.tail(100)[["date", "close"]].plot(x="date", kind="line", ax=axes, secondary_y=False)
+    df.tail(100)[["date", "atr"]].plot(x="date", kind="line", ax=axes, secondary_y=True)
+    fig.savefig("charts/average_true_range.png")
 
 def volume_features(df):
     """
@@ -297,6 +376,72 @@ def volume_features(df):
         pandas.DataFrame
     """
     df["volume_rolling"] = df["volume"] / df["volume"].shift(21)
-    # TODO on balance volume
-    # TODO chaikin oscilator
+    df = on_balance_volume(df)
+    df = chaikin_oscillator(df)
     return df
+
+def on_balance_volume(df):
+    """
+    Math reference: https://stockcharts.com/school/doku.php?id=chart_school:technical_indicators:on_balance_volume_obv
+    Args:
+        df: pandas.DataFrame, columns include at least ["close", "volume"]
+    Returns:
+        pandas.DataFrame
+    """
+    df["dollar_pnl"] = df["close"].shift(1) - df["close"]
+    df["on_balance_volume"] = df["volume"]
+    df["on_balance_volume"] = df.apply(lambda row: row.volume * -1 if row.dollar_pnl < 0 else row.volume, axis=1)
+    df["on_balance_volume"] = df["on_balance_volume"].cumsum()
+    # print(df.head(10)[["date", "close", "dollar_pnl", "volume", "on_balance_volume"]])
+    if produce_charts:
+        chart_on_balance_volume(df)
+    return df
+
+def chart_on_balance_volume(df):
+    """
+    Save chart to charts/on_balance_volume
+    Args:
+        df: pandas.DataFrame, columns include at least ["date", "close", "on_balance_volume"]
+    Returns:
+        None
+    """
+    fig, axes = plt.subplots(1, 1, figsize=(10, 10))
+    fig.tight_layout()
+    plt.suptitle("MSFT On Balance Volume (OBV)", fontsize=24)
+    plt.subplots_adjust(left=0.1, top=0.9, right=0.9, hspace = 0.4)
+    df.tail(100)[["date", "close"]].plot(x="date", kind="line", ax=axes, secondary_y=False)
+    df.tail(100)[["date", "on_balance_volume"]].plot(x="date", kind="line", ax=axes, secondary_y=True)
+    fig.savefig("charts/on_balance_volume.png")
+
+def chaikin_oscillator(df):
+    """
+    Math reference: https://stockcharts.com/school/doku.php?id=chart_school:technical_indicators:chaikin_oscillator
+    Args:
+        df: pandas.DataFrame, columns include at least ["low", "high", "close", "volume"]
+    Returns:
+        pandas.DataFrame
+    """
+    money_flow_multiplier = ((df["close"] - df["low"]) / (df["high"] - df["low"])) / (df["high"] - df["low"])
+    money_flow_volume = df["volume"] * money_flow_multiplier
+    adl = money_flow_volume.cumsum()
+    df["chaikin_oscillator"] = adl.ewm(com=(3-1)/2).mean() - adl.ewm(com=(10-1)/2).mean()
+    # print(df.head(10)[["date", "close", "volume", "chaikin_oscillator"]])
+    if produce_charts:
+        chart_chaikin_oscillator(df)
+    return df
+
+def chart_chaikin_oscillator(df):
+    """
+    Save chart to charts/chaikin_oscillator
+    Args:
+        df: pandas.DataFrame, columns include at least ["date", "close", "chaikin_oscillator"]
+    Returns:
+        None
+    """
+    fig, axes = plt.subplots(1, 1, figsize=(10, 10))
+    fig.tight_layout()
+    plt.suptitle("MSFT Chaikin Oscillator", fontsize=24)
+    plt.subplots_adjust(left=0.1, top=0.9, right=0.9, hspace = 0.4)
+    df.tail(100)[["date", "close"]].plot(x="date", kind="line", ax=axes, secondary_y=False)
+    df.tail(100)[["date", "chaikin_oscillator"]].plot(x="date", kind="line", ax=axes, secondary_y=True)
+    fig.savefig("charts/chaikin_oscillator.png")
--- ml-preprocessing/preprocessing.py
+++ ml-preprocessing/preprocessing.py
@@ -1,17 +1,48 @@
 import matplotlib
 import numpy as np
 import pandas as pd
+from sklearn import preprocessing as sklearn_preprocessing
+from sklearn.decomposition import PCA
 
 import generate_features
 
 def preprocessing():
     filename = "MSFT.csv"
-    df = source_to_df(filename)
+    df = yahoo_finance_source_to_df(filename)
     df = generate_features.return_features(df)
+    df = generate_features.target_value(df)
+    # print(df.head(5)[["date", "close", "volume", "return", "y"]])
     df = clean_df(df)
     df = new_features(df)
-    # print(df.head(5)[["date", "open", "high", "low", "close", "volume", "return"]])
-    df_to_csv(df=df, filename="MSFT_clean.csv")
+    X, y = remove_unused_features(df)
+    X = X.values # convert to np.ndarray for sklearn
+    y = y.values # convert to np.ndarray for sklearn
+    X = scaling(X)
+    X = dimensionality_reduction(X)
+    train_test_split = 7000
+    X_learn = X[:train_test_split]
+    y_learn = y[:train_test_split]
+    X_test = X[train_test_split:]
+    y_test = y[train_test_split:]
+    assert len(X_learn) + len(X_test) == len(X)
+    df_to_csv(df=pd.DataFrame(X_learn), filename="MSFT_X_learn.csv")
+    df_to_csv(df=pd.DataFrame(y_learn), filename="MSFT_y_learn.csv")
+    df_to_csv(df=pd.DataFrame(X_test), filename="MSFT_X_test.csv")
+    df_to_csv(df=pd.DataFrame(y_test), filename="MSFT_y_test.csv")
+
+def yahoo_finance_source_to_df(filename):
+    """
+    Lead Yahoo Finance csv and format to DataFrame
+    Args:
+        filename: string
+    Returns:
+        pandas.DataFrame
+    """
+    df = pd.read_csv(filename, header=0)
+    df.drop(labels="Close", axis=1, inplace=True)
+    df.columns = ["date", "open", "high", "low", "close", "volume"]
+    df["date"] = pd.to_datetime(df["date"])
+    return df
 
 def clean_df(df):
     df = missing_values(df)
@@ -24,10 +55,15 @@ def missing_values(df):
     """
     missing_values_count = df.isnull().sum()
     # print("Original data # missing values: {}".format(missing_values_count))
-    df = df.fillna(method = "ffill", axis=0).fillna("0")
-    missing_values_count = df.isnull().sum()
-    # print("After filling na # missing values: {}".format(missing_values_count))
-    return df
+    if sum(missing_values_count) == 0:
+        return df
+    else:
+        print("Ffill of missing values necessary")
+        df = df.fillna(method = "ffill", axis=0).fillna("0")
+        missing_values_count = df.isnull().sum()
+        # print("After filling na # missing values: {}".format(missing_values_count))
+        assert sum(missing_values_count) == 0
+        return df
 
 def outliers(df):
     df_outliers = df.loc[:,["date", "return", "close_to_open", "close_to_high", "close_to_low"]]
@@ -39,20 +75,71 @@ def outliers(df):
     return df
 
 def new_features(df):
+    """
+    Generate feature useful for learning
+    Args:
+        df: pandas.DataFrame
+    Returns:
+        pandas.DataFrame
+    """
     df = generate_features.trend_features(df)
     df = generate_features.momentum_features(df)
     df = generate_features.volatility_features(df)
     df = generate_features.volume_features(df)
     return df
 
-def source_to_df(filename):
-    df = pd.read_csv(filename, header=0)
-    df.drop(labels="Close", axis=1, inplace=True)
-    df.columns = ["date", "open", "high", "low", "close", "volume"]
-    df["date"] = pd.to_datetime(df["date"])
-    return df
+def remove_unused_features(df):
+    """
+    - skip rows with nan values due to feature extraction
+    - extract only useful columns for learning
+    Args:
+        df: pandas.DataFrame
+    Returns:
+        pandas.DataFrame
+    """
+    X = df.loc[200:len(df)-1, ["return", "close_to_open", "close_to_high", "close_to_low",
+        "macd_diff", "ma_50_200", "sar", "stochastic_oscillator",
+        "cci", "rsi", "5d_volatility", "21d_volatility", "60d_volatility",
+        "bollinger", "atr", "on_balance_volume", "chaikin_oscillator"]]
+    y = df.loc[200:len(df)-1, ["y"]]
+    return X, y
+
+def scaling(df):
+    """
+    Scale all features to unit variance and 0 mean to optimize training
+    Args:
+        df: pandas.DataFrame
+    Returns:
+        numpy.ndarray
+    """
+    X = sklearn_preprocessing.scale(df, axis=0)
+    assert sum(X.mean(axis=0)) > -0.00001 and sum(X.mean(axis=0)) < 0.00001 # zero mean
+    assert sum(X.std(axis=0)) > 0.99999 and sum(X.mean(axis=0)) < 1.00001 # unit variance
+    return X
+
+def dimensionality_reduction(X):
+    """
+    Analyzed the usefulness of dimensionality reduction
+    Args:
+        numpy.ndarray
+    Returns:
+        numpy.ndarray
+    """
+    sk_model = PCA(n_components=10)
+    sk_model.fit_transform(X)
+    sk_model.explained = sk_model.explained_variance_ratio_.cumsum()
+    # PCA not used due to a small number of original features
+    return X
 
 def df_to_csv(df, filename):
+    """
+    Save DataFrame to csv for later use
+    Args:
+        df: pandas.DataFrame
+        filename: string
+    Returns:
+        None
+    """
     df.to_csv(filename, index=False)
 
 if __name__ == "__main__":
