626d939618988721f6d7658bfbf6d3752d33dbcf
Adam Novotny <adam@adamnovotny.com>
2018-08-21 10:18:45 +0200

    feat: knapsack w/o rep

--- algos/knapsack_dynamic_programming.py
+++ algos/knapsack_dynamic_programming.py
@@ -20,3 +20,19 @@ def knapsack_with_repetition(items, weight):
 bag = knapsack_with_repetition(items, 10)
 print(bag)
 assert 48 == bag.loc[len(bag)-1]["value"]
+
+def knapsack_without_repetition(items, weight):
+    bag = pd.DataFrame(np.zeros((weight+1, len(items))))
+    for i in range(len(items)):
+        for j in range(1, weight+1):
+            item_w = items.loc[i, "weight"]
+            item_val = items.loc[i, "value"]
+            if j >= item_w:
+                val_new_w = bag.loc[j-item_w, i] + item_val
+                bag.loc[j, i] = max(bag.loc[j-1, i], val_new_w)
+            else:
+                bag.loc[j, i] = bag.loc[j-1, i]
+    return bag
+bag = knapsack_without_repetition(items, 10)
+print(bag)
+assert 45 == bag.loc[10, len(items)-1]
