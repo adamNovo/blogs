e9e20d5c239a144d3abce4bd3890a21a15e7f4cd
Adam Novotny <adam@adamnovotny.com>
2018-08-26 11:09:04 +0200

    init deployment: docker + heroku

---
+++ ml-deployment/flask/docker.py
@@ -0,0 +1,41 @@
+import argparse
+import os
+import subprocess
+import sys
+
+container = "flask_ml_app"
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-a", "--action", required=True, choices=["start", "stop", "rm", "ssh"], help="select action")
+args = vars(ap.parse_args())
+action = args["action"]
+
+def main():
+    if action == "start":
+        docker_start()
+    elif action == "stop":
+        docker_stop()
+    elif action == "rm":
+        docker_rm()
+    elif action == "ssh":
+        docker_ssh()
+    
+def docker_ssh():
+    subprocess.call("docker exec -it {} /bin/bash".format(container), shell=True)
+
+def docker_start():
+    subprocess.call("""
+    docker-compose -f docker-compose.yml up;""", shell=True)
+
+def docker_stop():
+    subprocess.call("""
+    docker stop {0};
+    """.format(container), shell=True)
+
+def docker_rm():
+    subprocess.call("""
+    docker rm {0};
+    """.format(container), shell=True)
+
+if __name__ == "__main__":
+    main()
